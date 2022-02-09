9133577ccca916f3ae7e691b4000dd2da3417734
Ronald Chan <the@gr8.ninja>
2022-02-09 17:11:09 +0800

    add logging example to HelloController

--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -1,5 +1,6 @@
 package com.example.demo;
 
+import org.slf4j.Logger;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -7,9 +8,16 @@ import org.springframework.web.bind.annotation.RequestParam;
 @RestController
 public class HelloController {
 
-	@GetMapping("/")
-	public String index(@RequestParam(value = "name", defaultValue = "World") String name) {
-		return String.format("Hello %s!", name);
+	private final Logger logger;
+
+	public HelloController(Logger logger) {
+		this.logger = logger;
 	}
 
+	@GetMapping("/")
+	public String index(@RequestParam(value = "name", defaultValue = "World") String name) {
+		logger.info("I said hello to {}", name);
+
+		return String.format("Hello %s!", name);
+	}
 }
