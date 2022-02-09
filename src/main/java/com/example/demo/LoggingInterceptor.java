2e181781e541048c9b0b8477f01ae623275ac70b
Ronald Chan <the@gr8.ninja>
2022-02-09 16:26:03 +0800

    implement request logging with WebFilter

---
+++ src/main/java/com/example/demo/LoggingInterceptor.java
@@ -0,0 +1,30 @@
+package com.example.demo;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import org.springframework.web.server.WebFilter;
+import org.springframework.web.server.WebFilterChain;
+
+import reactor.core.publisher.Mono;
+
+@Component
+public class LoggingInterceptor implements WebFilter {
+
+    private static final Logger logger = LoggerFactory.getLogger(LoggingInterceptor.class);
+
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, WebFilterChain chain) {
+        ServerHttpRequest req = exchange.getRequest();
+
+        final String method = req.getMethod().toString();
+        final String path = req.getPath().toString();
+        final String address = req.getRemoteAddress().toString();
+
+        logger.info("Request {} {} from IP {}", method, path, address);
+
+        return chain.filter(exchange);
+    }
+}
