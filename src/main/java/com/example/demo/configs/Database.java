84f4096730845c091eb39877466dfb7b91c01033
Ronald Chan <the@gr8.ninja>
2022-02-15 14:20:18 +0800

    use lombok annotation for configuration constructor

--- src/main/java/com/example/demo/configs/Database.java
+++ src/main/java/com/example/demo/configs/Database.java
@@ -3,19 +3,16 @@ package com.example.demo.configs;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.context.properties.ConstructorBinding;
 
+import lombok.RequiredArgsConstructor;
+
 /**
  * To set the configuration, specify environment variables DB_HOST, DB_PORT, DB_PASSWORD
  */
 @ConstructorBinding
+@RequiredArgsConstructor
 @ConfigurationProperties("db")
 public class Database {
     public final String host;
     public final String port;
     public final String password;
-
-    public Database(String host, String port, String password) {
-        this.host = host;
-        this.port = port; 
-        this.password = password;
-    }
 }
